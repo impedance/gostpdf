3c9f41c45e54d2da2a8fc87a1f9831f0e164406d
inozemtsev <a.inozemtsev@rosa.ru>
2025-11-25 15:30:33 +0300

    update

--- src/md2pdf/__init__.py
+++ src/md2pdf/__init__.py
@@ -4,13 +4,14 @@ from .bundle import DEFAULT_BUNDLE_METADATA, build, write_bundle
 from .config import ProjectConfig, load_config
 from .images import resolve_image_path, rewrite_images, strip_numeric
 from .pandoc_runner import render
-from .reporting import StructureWarning
+from .reporting import StructureWarning, format_warnings, write_warnings
 from .walker import walk
 
 __all__ = [
     "DEFAULT_BUNDLE_METADATA",
     "ProjectConfig",
     "StructureWarning",
+    "format_warnings",
     "build",
     "write_bundle",
     "load_config",
@@ -19,4 +20,5 @@ __all__ = [
     "rewrite_images",
     "strip_numeric",
     "walk",
+    "write_warnings",
 ]
