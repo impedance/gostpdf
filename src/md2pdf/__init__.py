44f67c9099d76d285bf24479e35c1643c9dd2380
a.inozemtsev <2275399@gmail.com>
2025-11-25 15:19:56 +0300

    Add pandoc runner

--- src/md2pdf/__init__.py
+++ src/md2pdf/__init__.py
@@ -3,6 +3,7 @@
 from .bundle import DEFAULT_BUNDLE_METADATA, build
 from .config import ProjectConfig, load_config
 from .images import resolve_image_path, rewrite_images, strip_numeric
+from .pandoc_runner import render
 from .reporting import StructureWarning
 from .walker import walk
 
@@ -12,6 +13,7 @@ __all__ = [
     "StructureWarning",
     "build",
     "load_config",
+    "render",
     "resolve_image_path",
     "rewrite_images",
     "strip_numeric",
